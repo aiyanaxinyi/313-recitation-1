a5c8772ac63327cea2ac633a00addf35e01067db
aashaiavadhani <aavadhan@andrew.cmu.edu>
2020-09-04 11:17:43 -0700

    Added my name

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("3 years - Hello, World");
+        System.out.println("Partner: Aashai Avadhani");
     }
 
 }
